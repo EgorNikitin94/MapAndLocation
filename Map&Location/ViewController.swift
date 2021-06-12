d51c0ab8bde85d9ff081b86b4944bc1d3c914ea1
Егор Никитин <Egornik1994@yandex.ru>
2021-06-12 14:51:19 +0300

    Initial Commit

---
+++ Map&Location/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Map&Location
+//
+//  Created by Егор Никитин on 12.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
